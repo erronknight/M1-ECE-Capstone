270b4a3e74c525419f3e5d09a28074c9dd9a7ca4
Jason Allen <jason@allenhub.com>
2020-11-14 11:09:06 -0800

    update the map data subscribers to handle multiple turtlebots

--- map_data/src/goal_listener.py
+++ map_data/src/goal_listener.py
@@ -3,9 +3,17 @@
 import rospy
 from geometry_msgs.msg import PoseStamped
 
+turtlebot_dict = {
+	"turtlebot1" : "tb3_0/move_base_simple/goal",
+	"turtlebot2" : "tb3_1/move_base_simple/goal",
+	"turtlebot3" : "tb3_2/move_base_simple/goal",
+	"turtlebot4" : "tb3_3/move_base_simple/goal"
+}
+
 class GoalSubscriber(object):
-	def __init__(self):
-		self.sub = rospy.Subscriber('move_base_simple/goal', PoseStamped, self.sub_callback)
+	def __init__(self, tb3_name):
+		topic = turtlebot_dic[tb3_name]
+		self.sub = rospy.Subscriber(topic, PoseStamped, self.sub_callback)
 		self.goaldata = PoseStamped()
 
 	def sub_callback(self, msg):
@@ -14,6 +22,6 @@ class GoalSubscriber(object):
 
 if __name__ == "__main__":
 	rospy.init_node("goal_subscriber_node", anonymous=True)
-	goal_subscriber = GoalSubscriber()
+	goal_subscriber = GoalSubscriber("turtlebot1")
 	rospy.spin()
 
